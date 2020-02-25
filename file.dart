fceb570c82d80ef2f874ce4f3544d99c79b06230
Cas van Gool <cas.vangool@strukton.com>
2020-02-25 19:41:10 +0100

    dart file 4_numbers

--- file.dart
+++ file.dart
@@ -1,11 +1,12 @@
 void main(){
- String hellotext = "Hello world!!!";
- print(hellotext);
- double number = 0.4;
- print(number);
- number = 5;
- print(number);
- bool boolean = true;
- boolean = false;
- print(boolean);
+  int a = 3;
+  int b = 2;
+  int c = a *2;
+  print(c);
+  int d = a - b;
+  int e = a + c;
+  print(d);
+  print(e);
+  a += 2;
+  print(a);
 }
