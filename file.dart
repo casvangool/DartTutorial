d121db133f9655f82fbc342a1680ff5ef2e73ee4
Cas van Gool <cas.vangool@strukton.com>
2020-02-25 20:11:01 +0100

    dart file 8_booleans

---
+++ file.dart
@@ -0,0 +1,22 @@
+void main() {
+  bool boolean = true;
+  boolean = false;
+  print(boolean);
+  int a = 5;
+  int b = 4;
+  int c = 4;
+  boolean = a == b;
+  print(boolean);
+  boolean = a != b;
+  print(boolean);
+  boolean = a < b;
+  print(boolean);
+  boolean = a >= b;
+  print(boolean);
+  String texta = "Hi";
+  String textb = "hi";
+  boolean = texta == textb;
+  print(boolean);
+  boolean = a != b || b == a;
+  print(boolean);
+}
