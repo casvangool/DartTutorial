6565b08ff22e883c507c32cc6c7047d415a2fe21
Cas van Gool <cas.vangool@strukton.com>
2020-02-25 19:39:18 +0100

    dart file explicit_typing

---
+++ file.dart
@@ -0,0 +1,11 @@
+void main(){
+ String hellotext = "Hello world!!!";
+ print(hellotext);
+ double number = 0.4;
+ print(number);
+ number = 5;
+ print(number);
+ bool boolean = true;
+ boolean = false;
+ print(boolean);
+}
